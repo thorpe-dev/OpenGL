00f78ec85689d1d061e307a49c90c4e61e9320c8
Michael Thorpe <michael.thorpe09@imperial.ac.uk>
2012-02-22 16:17:54 -0800

    Commiting because wtf c++, remaking this vm from scratch

---
+++ src/CMakeFiles/vtk.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/vtk.dir/cgRender.cpp.o"
+  "../bin/vtk.pdb"
+  "../bin/vtk"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/vtk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
